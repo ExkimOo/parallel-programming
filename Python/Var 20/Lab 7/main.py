2e4f2edda3a0422433e371cd04f2d20ab7417cd3
Egor <bamditse@gmail.com>
2022-02-26 16:42:18 +0700

    Added Lab 20.7 and Lab 21.5

---
+++ Python/Var 20/Lab 7/main.py
@@ -0,0 +1,129 @@
+from threading import Thread
+from random import randint
+from time import sleep
+
+from rwlock import RwLock
+
+rwlock = RwLock()
+
+
+class Rectangle():
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.x2 = x2
+        if x1 == x2:
+            self.x2 = x2 + 1
+        if x1 > x2:
+            self.x1 = x2
+            self.x2 = x1
+
+        self.y1 = y1
+        self.y2 = y2
+        if y1 == y2:
+            self.y2 = y2 + 1
+        if y1 > y2:
+            self.y1 = y2
+            self.y2 = y1
+
+        self.area = abs(self.y1 - self.y2) * abs(self.x1 - self.x2)
+
+    def __repr__(self):
+        return repr(f'Rectangle: (x1={self.x1}, y1={self.y1}, x2={self.x2}, y2={self.y2}), area: {self.area}')
+
+
+rectangles = [Rectangle(randint(0, 15), randint(0, 15), randint(0, 15), randint(0, 15)) for _ in range(2)]
+
+
+def main():
+    for func in [delete_rectangle,
+                 add_rectangle,
+                 print_rectangles,
+                 print_max_rectangle_area,
+                 print_disjoint_rectangles]:
+        thread = Thread(target=func)
+        thread.start()
+
+
+def delete_rectangle():
+    global rectangles
+    global lock
+
+    while True:
+        rwlock.acquire_w()
+        i = randint(0, len(rectangles))
+        print(f'[Removed]'.ljust(22) + f' {i}')
+        rectangles = rectangles[:i] + rectangles[i + 1:]
+        rwlock.release_w()
+
+        sleep(2.5)
+
+
+def add_rectangle():
+    global rectangles
+    global lock
+
+    while True:
+        rwlock.acquire_w()
+        i = randint(0, len(rectangles))
+        generated_rect = Rectangle(randint(0, 5), randint(0, 5), randint(0, 5), randint(0, 5))
+        rectangles.append(Rectangle(randint(0, 5), randint(0, 5), randint(0, 5), randint(0, 5)))
+        print(f'[Added]'.ljust(22) + f' {generated_rect}')
+        rwlock.release_w()
+
+        sleep(2.5)
+
+
+def print_rectangles():
+    global rectangles
+    global lock
+
+    while True:
+        rwlock.acquire_r()
+        print(f'[Printed]'.ljust(22) + f' {rectangles}')
+        rwlock.release_r()
+
+        sleep(2.5)
+
+
+def print_max_rectangle_area():
+    global rectangles
+    global lock
+
+    while True:
+        rwlock.acquire_r()
+        max_area_rect = rectangles[0]
+        for rect in rectangles:
+            if max_area_rect.area < rect.area:
+                max_area_rect = rect
+
+        print(f'[Max area rectangles]'.ljust(22) + f' {max_area_rect}')
+        rwlock.release_r()
+
+        sleep(2.5)
+
+
+def print_disjoint_rectangles():
+    global rectangles
+    global lock
+
+    while True:
+        rwlock.acquire_r()
+        count = 0
+        for i in range(len(rectangles) - 1):
+            for j in range(i + 1, len(rectangles)):
+                if not (((rectangles[i].x1 >= rectangles[j].x1 and rectangles[i].x2 <= rectangles[j].x1) or
+                         (rectangles[i].x1 >= rectangles[j].x2 and rectangles[i].x2 <= rectangles[j].x2)) and
+                        ((rectangles[i].y1 >= rectangles[j].y1 and rectangles[i].y2 <= rectangles[j].y1) or
+                         (rectangles[i].y1 >= rectangles[j].y2 and rectangles[i].y2 <= rectangles[j].y2))):
+                    print(f'[Disjoint]'.ljust(22) + f' {rectangles[i]}, {rectangles[j]}')
+                    count += 1
+                    if count > 3:
+                        break
+
+        rwlock.release_r()
+
+        sleep(2.5)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Python/Var 21/Lab 5/main.py
@@ -0,0 +1,71 @@
+from threading import Thread, Lock, Semaphore
+from random import randint
+from time import sleep
+
+N = 5
+M = 3
+passengers = [randint(1, 3) for _ in range(N)]
+buses = [[i, randint(0, N - 1), 0] for i in range(M)]
+
+change_mutex = Lock()
+print_mutex = Lock()
+sem = Semaphore(M)
+sem.release()
+
+
+def main():
+    buses_thread = [None] * M
+    for i in range(M):
+        buses_thread[i] = Thread(target=stop_bus, args=(i,))
+        buses_thread[i].start()
+
+    passengers_thread = [None] * N
+    for i in range(N):
+        passengers_thread[i] = Thread(target=board_passengers, args=(i,))
+        passengers_thread[i].start()
+
+
+def stop_bus(num_thread):
+    global passengers
+    global buses
+
+    while True:
+        change_mutex.acquire()
+        passengers_to_board = randint(-buses[num_thread][2], passengers[buses[num_thread][1]])
+        buses[num_thread][2] += passengers_to_board
+        passengers[buses[num_thread][1]] -= passengers_to_board
+
+        print_mutex.acquire()
+        print(f'Bus #{buses[num_thread][0]} at stop #{buses[num_thread][1]} with {buses[num_thread][2]} passengers. ',
+              end='')
+        if passengers_to_board >= 0:
+            print(f'{passengers_to_board} have boarded to bus #{buses[num_thread][0]}')
+        else:
+            print(f'{-passengers_to_board} have out of bus #{buses[num_thread][0]}')
+        print_mutex.release()
+        change_mutex.release()
+
+        sem.acquire()
+        sleep(2.9)
+
+
+def board_passengers(num_thread):
+    global passengers
+    global buses
+
+    while True:
+        print_mutex.acquire()
+        print(f'Stop {num_thread} with {passengers[num_thread]} passengers')
+        print_mutex.release()
+
+        change_mutex.acquire()
+        for i in range(len(buses)):
+            buses[i][1] = randint(0, N - 1)
+        change_mutex.release()
+
+        sem.release()
+        sleep(3)
+
+
+if __name__ == '__main__':
+    main()
