20e3568f187e549ac3542d8d41fc5a64c9c9a327
Egor <bamditse@gmail.com>
2022-03-03 13:42:18 +0700

    Added Lab 20.10 and Lab 21.10

---
+++ Python/Var 20/Lab 10/kernel.cu
@@ -0,0 +1,116 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+__global__ void checkMagic(int* matrix, int N, int e);
+
+int main()
+{
+    srand(time(NULL));
+
+    int N = 0;
+    int e = 0;
+
+    std::cout << "Enter N: ";
+    std::cin >> N;
+
+    std::cout << "Enter e: ";
+    std::cin >> e;
+
+    int* matrix = new int[N * N];
+
+    for (int i = 0; i < N * N; i++)
+    {
+        matrix[i] = rand() % 5;
+    }
+
+    std::cout << "Matrix:" << std::endl;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            std::cout << matrix[i*N+j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
+    int* m;
+    cudaMalloc((void**)&m, (N * N) * sizeof(int));
+    cudaMemcpy(m, matrix, (N * N) * sizeof(int), cudaMemcpyHostToDevice);
+
+    checkMagic << < 1, 2 >> > (m, N, e);
+
+    cudaFree(m);
+    return 0;
+}
+
+__global__ void checkMagic(int* matrix, int N, int e)
+{
+    int idx = threadIdx.x;
+
+    int* horSums = new int[N];
+    int* verSums = new int[N];
+
+    if (idx == 0)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < N; j++)
+            {
+                sum += matrix[i * N + j];
+            }
+            horSums[i] = sum;
+        }
+
+        printf("Horizontal sums: ");
+        for (int i = 0; i < N; i++)
+        {
+            printf("%d ", horSums[i]);
+        }
+        printf("\n");
+    }
+
+    if (idx == 1)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < N; j++)
+            {
+                sum += matrix[j * N + i];
+            }
+            verSums[i] = sum;
+        }
+
+        printf("Vertical sums: ");
+        for (int i = 0; i < N; i++)
+        {
+            printf("%d ", verSums[i]);
+        }
+        printf("\n");
+    }
+
+    __syncthreads();
+
+    if (idx == 0)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (!(horSums[i] - verSums[j] < e))
+                {
+                    printf("Not magic");
+                    return;
+                }
+            }
+        }
+        printf("Magic");
+    }
+}
---
+++ Python/Var 21/Lab 10/kernel.cu
@@ -0,0 +1,89 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+__global__ void createMatrix(double* matrix, int stepsX, double x1, double y1, double h);
+double checkInput();
+
+int main()
+{
+    double x1, y1, x2, y2, h;
+    std::cout << "x1: ";
+    x1 = checkInput();
+    std::cout << "x2: ";
+    x2 = checkInput();
+    std::cout << "y1: ";
+    y1 = checkInput();
+    std::cout << "y2: ";
+    y2 = checkInput();
+    std::cout << "Step h: ";
+    h = checkInput();
+
+    if (x1 > x2)
+    {
+        std::swap(x1, x2);
+    }
+
+    if (y1 > y2)
+    {
+        std::swap(y1, y2);
+    }
+
+    int stepsX = int((x2 - x1) / h) + 1;
+    int stepsY = int((y2 - y1) / h) + 1;
+
+    double* matrix = new double[stepsX * stepsY];
+
+    double x = x1;
+    double y = y1;
+
+    double* m;
+    cudaMalloc((void**)&m, (stepsX * stepsY) * sizeof(double));
+
+    createMatrix << < 1, stepsX * stepsY >> > (m, stepsX, x1, y1, h);
+
+    cudaMemcpy(matrix, m, (stepsX * stepsY) * sizeof(double), cudaMemcpyDeviceToHost);
+
+    std::cout << "Matrix:" << std::endl;
+    for (int i = 0; i < stepsX; i++)
+    {
+        for (int j = 0; j < stepsX; j++)
+        {
+            std::cout << matrix[i * stepsX + j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
+    cudaFree(m);
+    return 0;
+}
+
+__global__ void createMatrix(double* m, int stepsX, double x1, double y1, double h)
+{
+    int idx = threadIdx.x;
+    double x = x1 + (idx / stepsX) * h;
+    double y = y1 + (idx % stepsX) * h;
+
+    m[idx] = (y - x) * y * cosf(x);
+}
+
+double checkInput() 
+{
+    double a = 0;
+    do
+    {
+        if (!std::cin)
+        {
+            std::cin.clear();
+            while (std::cin.get() != '\n');
+            std::cout << "Wrong value, try again\n";
+        }
+        std::cin >> a;
+    } while (!std::cin);
+
+    return a;
+}
